7d7bca98dc6799d56852001a493fdc12e20fae98
Ethan Duwell <eduwell@mcw.edu>
2023-08-04 16:09:23 -0500

    Add files via upload

---
+++ install/pyannote/pythonCode/wsprPyannote2.py
@@ -0,0 +1,15 @@
+import wsprPyannote1
+import sys
+
+#inFile="/mnt/76cfc2bb-b830-47a1-9a9c-0d28b8a1efab/python_projects/installTesting/latest/dataScraping/input/JimmyKimmelAsksPresidentBarackObamaAboutHisDailyLife-QmPLGt5rd_k.mp4"
+#OutDir="/mnt/76cfc2bb-b830-47a1-9a9c-0d28b8a1efab/python_projects/installTesting/latest/dataScraping/output"
+#OutDir_sub="test43"
+
+
+inFile = sys.argv[1];
+OutDir = sys.argv[2];
+OutDir_sub = sys.argv[3];
+tokenIn = sys.argv[4];
+installDir = sys.argv[5];
+
+wsprPyannote1.run(inFile,OutDir,OutDir_sub,tokenIn,installDir)
